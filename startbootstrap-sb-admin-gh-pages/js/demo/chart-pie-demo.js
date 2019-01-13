91bb223f6f4d33c090da1fb609df91a8c79d06b5
DESKTOP-74HVT24\xdavi <xdavid89x@gmail.com>
2019-01-12 20:14:54 -0800

    2nd

--- startbootstrap-sb-admin-gh-pages/js/demo/chart-pie-demo.js
+++ startbootstrap-sb-admin-gh-pages/js/demo/chart-pie-demo.js
@@ -4,13 +4,27 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
+var waste = 40;
+var recycle = 35;
+var compost = 25;
+
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
     labels: ["Waste", "Recycle", "Compost"],
     datasets: [{
-      data: [50, 25, 25],
+      data: [waste, recycle, compost],
       backgroundColor: ['#007bff', '#dc3545', '#ffc107'],
     }],
   },
 });
+
+var status = document.getElementById("status");
+var messageStat = document.getElementById("messageStat");
+if (waste >= 50) {
+  $("#status").html("Your waste is high");
+  $("#status").css("color", "red");
+} else {
+  $("#status").html("Your waste is low. Keep up the good work!");
+  $("#status").css("color", "green");
+}
